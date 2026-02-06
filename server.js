99580562b92f70b6cee56cb6d400dc09fb2c715c
saravanan574 <arulsaravanan461@gmail.com>
2026-02-06 23:11:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const connectDB = require("./config/db");
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin:"https://money-manager-frontend-2uxb.onrender.com"
+}));
 app.use(express.json());
 
 app.use("/api/transactions", require("./routes/Transaction"));
